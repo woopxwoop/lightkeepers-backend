85b582c0f9213529d8a6d5d7acbd4539f409905c
An Lou <louan1734@gmail.com>
2025-12-27 20:37:13 -0600

    cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.models import Character, AbyssTeam
 from app.wrapper import YSHelperWrapper
 from app.get import get_abyss_teams
@@ -7,6 +8,20 @@ app = FastAPI(
   title="Genshin Team Recommender Wrapper",
   version="0.1.0",
   description="Backend wrapper normalizing YSHelper data"
+)
+
+origins = [
+  "https://woopxwoop.github.io/portfolio/",
+  "http://localhost",
+  "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # List of allowed origins
+    allow_credentials=True,         # Allow cookies to be included in cross-origin requests
+    allow_methods=["*"],            # Allow all standard methods (GET, POST, PUT, etc.)
+    allow_headers=["*"],            # Allow all headers
 )
   
 @app.get("/")
